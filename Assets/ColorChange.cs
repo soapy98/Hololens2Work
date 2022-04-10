a97c83068a1a7b985442c649cc2b513d32c575c0
Sohail Turner <sohailturner@gmail.com>
2022-04-10 23:12:33 +0100

    MixedReality

---
+++ Assets/ColorChange.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    public MeshRenderer mesh;
+    [SerializeField] Material Selected;
+    Material Original;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        Original = mesh.material;
+    }
+    public void OnEyeGaze()
+    {
+        mesh.material = Selected;
+    }
+
+    public void OffEyeGaze()
+    {
+        mesh.material = Original;
+    }
+
+}
---
+++ Assets/SpeechRecognition.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit;
+using UnityEngine;
+
+public class SpeechRecognition : MonoBehaviour, IMixedRealitySpeechHandler
+{
+    public GameObject cable;
+    public GameObject controller;
+    public ColorChange col;
+    public GameObject end;
+    public Vector3 origPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CoreServices.InputSystem?.RegisterHandler<IMixedRealitySpeechHandler>(this);
+        col = cable.GetComponent<ColorChange>();
+        origPos = cable.transform.position;
+    }
+    public void OnSpeechKeywordRecognized(SpeechEventData eventData)
+    {
+        float step = 2 * Time.deltaTime;
+        switch (eventData.Command.Keyword.ToLower())
+        {
+            case "clockwise":
+                cable.transform.position = Vector3.MoveTowards(cable.transform.position, end.transform.position, step);
+                controller.transform.Rotate(0, 0, 1000 * Time.deltaTime);
+                controller.GetComponent<ColorChange>().OnEyeGaze();
+                break;
+            case "anticlockwise":
+                cable.transform.position = Vector3.MoveTowards(cable.transform.position, origPos, step);
+                controller.transform.Rotate(0, 0, -1000 * Time.deltaTime);
+                controller.GetComponent<ColorChange>().OffEyeGaze();
+                break;
+            case "hello":
+
+                Debug.Log("Hello World");
+
+                break;
+
+            default:
+
+                Debug.Log($"Unkown Option{eventData.Command.Keyword}");
+
+                break;
+
+        }
+
+    }
+}
